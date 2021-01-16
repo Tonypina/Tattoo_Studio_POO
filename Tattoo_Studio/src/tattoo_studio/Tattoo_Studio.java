e9fcfff8263f47a403c81f8f22e32db67e11c0b4
Marco Antonio Piña Rossette <65735766+Tonypina@users.noreply.github.com>
2021-01-16 17:58:17 -0600

    Update Tattoo_Studio.java

--- Tattoo_Studio/src/tattoo_studio/Tattoo_Studio.java
+++ Tattoo_Studio/src/tattoo_studio/Tattoo_Studio.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tattoo_studio;
 
 /**
@@ -15,7 +10,7 @@ public class Tattoo_Studio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
