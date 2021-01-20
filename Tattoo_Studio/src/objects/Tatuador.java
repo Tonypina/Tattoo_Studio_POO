e0948ec92d75e0213a89c960ed709a4222766194
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-01-20 14:05:08 -0600

    Add files via upload

--- Tattoo_Studio/src/objects/Tatuador.java
+++ Tattoo_Studio/src/objects/Tatuador.java
@@ -10,5 +10,57 @@ package objects;
  * @author
  */
 public class Tatuador {
+    private String nombre;
+    private String ap_pat;
+    private String ap_mat;
+    private int contatco;
+    private float comision;
+
+    public Tatuador(String nombre, String ap_pat, String ap_mat, int contatco, float comision) {
+        this.nombre = nombre;
+        this.ap_pat = ap_pat;
+        this.ap_mat = ap_mat;
+        this.contatco = contatco;
+        this.comision = comision;
+    }
     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAp_pat() {
+        return ap_pat;
+    }
+
+    public String getAp_mat() {
+        return ap_mat;
+    }
+
+    public int getContatco() {
+        return contatco;
+    }
+
+    public float getComision() {
+        return comision;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAp_pat(String ap_pat) {
+        this.ap_pat = ap_pat;
+    }
+
+    public void setAp_mat(String ap_mat) {
+        this.ap_mat = ap_mat;
+    }
+
+    public void setContatco(int contatco) {
+        this.contatco = contatco;
+    }
+
+    public void setComision(float comision) {
+        this.comision = comision;
+    }
 }
