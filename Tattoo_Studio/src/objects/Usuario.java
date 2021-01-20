ab9375940ef779debd0865ae88ddcc1100ff5c1b
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-01-20 16:19:09 -0600

    Add files via upload

--- Tattoo_Studio/src/objects/Usuario.java
+++ Tattoo_Studio/src/objects/Usuario.java
@@ -11,4 +11,67 @@ package objects;
  */
 public class Usuario {
     
+    private int id;
+    private String nombre;
+    private String ap_paterno;
+    private String ap_materno;
+    private String username;
+    private String pass;
+    
+    public Usuario(int id, String nombre, String ap_paterno, String ap_materno, String username, String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ap_paterno = ap_paterno;
+        this.ap_materno = ap_materno;
+        this.username = username;
+        this.pass = pass;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAp_paterno(String ap_paterno) {
+        this.ap_paterno = ap_paterno;
+    }
+
+    public void setAp_materno(String ap_materno) {
+        this.ap_materno = ap_materno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAp_paterno() {
+        return ap_paterno;
+    }
+
+    public String getAp_materno() {
+        return ap_materno;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+        
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
 }
