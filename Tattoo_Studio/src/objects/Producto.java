f120b0fdb20a48727d6ccc0f6d4ac05d0ba92d42
AremPOO <77461400+AremPOO@users.noreply.github.com>
2021-01-20 19:33:35 -0600

    Update Producto.java

--- Tattoo_Studio/src/objects/Producto.java
+++ Tattoo_Studio/src/objects/Producto.java
@@ -63,30 +63,7 @@
     public String getProvedor(){
         return provedor;
     }
-    
-
+   
 
 }
 
-
-class Main{
-    public static void main(String [] e){
-
-        Producto pro = new Producto(7,"Merceddes","Electrico",57,100000.5,"Pumas UNAM");
-        pro.setIdPro(7);
-        pro.setModeloPro("Mercedes");
-        pro.setTipoPro("Electrico");
-        pro.setCantidadPro(57);
-        pro.setPrecioPro(100000.5);
-        pro.setProvedor("Pumas UNAM");
-        System.out.println(pro.getIdPro()+"\n");
-        System.out.println(pro.getModeloPro()+"\n");
-        System.out.println(pro.getTipoPro()+"\n");
-        System.out.println(pro.getCantidadPro()+"\n");
-        System.out.println(pro.getPrecioPro()+"\n");
-        System.out.println(pro.getProvedor()+"\n");
-        //Producto p = new Producto(p.getIdPro(), p.getModeloPro(), p.getTipoPro(), p.getCantidadPro(), p.getPrecioPro(), p.getProvedor());
-        
-        //System.out.println(p);
-    }
-}
