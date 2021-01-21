344af720d686dd4b02b38cc811bb183868c2a011
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-01-20 19:04:26 -0600

    Add files via upload

--- Tattoo_Studio/src/objects/Cita.java
+++ Tattoo_Studio/src/objects/Cita.java
@@ -4,58 +4,57 @@
  * and open the template in the editor.
  */
 package objects;
-import java.util.*;
 /**
  *
  * @author 
  */
 public class Cita {
-    private Calendar fechaInicio;
-    private Calendar fechaFinal;
+    private int diaInicio;
+    private int mesInicio;
+    private int anioInicio;
+    private int diaFinal;
+    private int mesFinal;
+    private int anioFinal;
     private String nombreCliente;
     private float anticipo;
     private float precio;
     private int duración; //En minutos
 
-    public Cita(Calendar fechaFinal, Calendar fechaInicio, String nombreCliente, float anticipo, float precio, int duración) {
-        this.fechaInicio = fechaInicio;
-        this.fechaFinal = fechaFinal;
+    public Cita(int diaInicio, int mesInicio, int anioInicio, int diaFinal, int mesFinal, int anioFinal, String nombreCliente, float anticipo, float precio, int duración) {
+        this.diaInicio = diaInicio;
+        this.mesInicio = mesInicio;
+        this.anioInicio = anioInicio;
+        this.diaFinal = diaFinal;
+        this.mesFinal = mesFinal;
+        this.anioFinal = anioFinal;
         this.nombreCliente = nombreCliente;
         this.anticipo = anticipo;
         this.precio = precio;
         this.duración = duración;
     }
 
-    public void setFechaInicio(Calendar fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-    
-    public void setFechaFinal(Calendar fechaFinal){
-        this.fechaFinal = fechaFinal;
-    }
-    
-    public void setNombreCliente(String nombreCliente) {
-        this.nombreCliente = nombreCliente;
+    public int getDiaInicio() {
+        return diaInicio;
     }
 
-    public void setAnticipo(float anticipo) {
-        this.anticipo = anticipo;
+    public int getMesInicio() {
+        return mesInicio;
     }
 
-    public void setPrecio(float precio) {
-        this.precio = precio;
+    public int getAnioInicio() {
+        return anioInicio;
     }
 
-    public void setDuración(int duración) {
-        this.duración = duración;
+    public int getDiaFinal() {
+        return diaFinal;
     }
 
-    public Calendar getFechaInicio() {
-        return fechaInicio;
+    public int getMesFinal() {
+        return mesFinal;
     }
-    
-    public Calendar getFechaFinal() {
-        return fechaFinal;
+
+    public int getAnioFinal() {
+        return anioFinal;
     }
 
     public String getNombreCliente() {
@@ -72,5 +71,45 @@ public class Cita {
 
     public int getDuración() {
         return duración;
+    }
+
+    public void setDiaInicio(int diaInicio) {
+        this.diaInicio = diaInicio;
+    }
+
+    public void setMesInicio(int mesInicio) {
+        this.mesInicio = mesInicio;
+    }
+
+    public void setAnioInicio(int anioInicio) {
+        this.anioInicio = anioInicio;
+    }
+
+    public void setDiaFinal(int diaFinal) {
+        this.diaFinal = diaFinal;
+    }
+
+    public void setMesFinal(int mesFinal) {
+        this.mesFinal = mesFinal;
+    }
+
+    public void setAnioFinal(int anioFinal) {
+        this.anioFinal = anioFinal;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public void setAnticipo(float anticipo) {
+        this.anticipo = anticipo;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void setDuración(int duración) {
+        this.duración = duración;
     }
 }
