62d2073f6e8f520727a2dde421e7af0bd823b332
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2021-01-23 14:32:00 -0600

    Add files via upload

---
+++ src/objects/Socio.java
@@ -0,0 +1,36 @@
+package objects;
+
+public class Socio{
+  private String nombre;
+  private String contacto;
+  private boolean rango;
+
+  public Socio(String nombre, String contacto, boolean rango){
+    this.nombre = nombre;
+    this.contacto = contacto;
+    this.rango = rango;
+
+  }
+  public String getNombre() {
+        return nombre;
+  }
+
+  public String getContacto() {
+        return contacto;
+  }
+
+  public String getRango() {
+        return rango;
+  }
+
+  public String setNombre() {
+        this.nombre = nombre;
+  }
+
+  public String setContacto() {
+        this.contacto = contacto;
+  }
+
+  public String setRango() {
+        this.rango = rango;
+  }
