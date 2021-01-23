05b57df4b2eaf68b1bee0c1e2dbde19ee613a88f
Miriam Reyes <54131192+miriamyi01@users.noreply.github.com>
2021-01-23 14:50:38 -0600

    Update Socio.java

--- src/objects/Socio.java
+++ src/objects/Socio.java
@@ -4,34 +4,35 @@ public class Socio{
   private String nombre;
   private String contacto;
   private boolean rango;
-  private double total;
 
   public Socio(String nombre, String contacto, boolean rango){
-    this.nombre = nombre;
-    this.contacto = contacto;
-    this.rango = rango;
+    this.setNombre(nombre);
+    this.setContacto(contacto);
+    this.setRango(rango);
 
   }
-  public String getNombre() {
-        return nombre;
-  }
 
-  public String getContacto() {
-        return contacto;
-  }
-
-  public String getRango() {
-        return rango;
-  }
-
-  public String setNombre() {
-        this.nombre = nombre;
-  }
-
-  public String setContacto() {
-        this.contacto = contacto;
-  }
-
-  public String setRango() {
-        this.rango = rango;
-  }
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getContacto() {
+		return contacto;
+	}
+	
+	public void setContacto(String contacto) {
+		this.contacto = contacto;
+	}
+	
+	public boolean isRango() {
+		return rango;
+	}
+	
+	public void setRango(boolean rango) {
+		this.rango = rango;
+	}
+}
