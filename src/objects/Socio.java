89b7690fcf624bc2285bc4174ac1600527f5706e
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-01-25 16:47:01 -0600

    Add files via upload

--- src/objects/Socio.java
+++ src/objects/Socio.java
@@ -1,52 +1,50 @@
 package objects;
 
 public class Socio{
-  private int id;
-  private String nombre;
-  private String contacto;
-  private boolean rango;
+    private int id;
+    private String nombre;
+    private String contacto;
+    private boolean rango;
 
-  public Socio(String nombre, String contacto, boolean rango){
-    this.setNombre(nombre);
-    this.setContacto(contacto);
-    this.setRango(rango);
+    public Socio(String nombre, String contacto, boolean rango){
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.rango = rango;
+    }
 
-  }
+    // Sobrecarga de constructor para base de datos
+    public Socio(int id, String nombre, String contacto, boolean rango){
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.rango = rango;
+    }
 
-  // Sobrecarga de constructor para base de datos
-  public Socio(int id, String nombre, String contacto, boolean rango){
-    this.id = id;
-    this.setNombre(nombre);
-    this.setContacto(contacto);
-    this.setRango(rango);
-
-  }
-
-	public int getId(){
-		return id;
-	}
+    public int getId(){
+        return id;
+    }
 	
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 	
-	public String getContacto() {
-		return contacto;
-	}
+    public String getContacto() {
+    	return contacto;
+    }
 	
-	public void setContacto(String contacto) {
-		this.contacto = contacto;
-	}
+    public void setContacto(String contacto) {
+    	this.contacto = contacto;
+    }
 	
-	public boolean isRango() {
-		return rango;
-	}
+    public boolean isRango() {
+    	return rango;
+    }
 	
-	public void setRango(boolean rango) {
-		this.rango = rango;
-	}
+    public void setRango(boolean rango) {
+    	this.rango = rango;
+    }
 }
--- src/objects/Tatuador.java
+++ src/objects/Tatuador.java
@@ -1,87 +1,96 @@
 package objects;
 
 public class Tatuador{
-  private int id;
-  private String nombre;
-  private String ap_pat;
-  private String ap_mat;
-  private String contacto;
-  private int rangoInt;
-  private boolean Principal;
-  private boolean Aprendiz;
-  private boolean Secundario;
-  public boolean[] RangoArr = new boolean [3];
-  boolean[] Rango = {Principal, Aprendiz, Secundario};
+    private int id;
+    private String nombre;
+    private String ap_pat;
+    private String ap_mat;
+    private String contacto;
+    private int rangoInt;
+    private boolean Principal;
+    private boolean Aprendiz;
+    private boolean Secundario;
+    public boolean[] RangoArr = new boolean [3];
+    boolean[] Rango = {Principal, Aprendiz, Secundario};
   
-  public Tatuador(String nombre, String ap_pat, String ap_mat, String contacto, int rangoInt){
-    this.nombre = nombre;
-    this.ap_pat = ap_pat;
-    this.ap_mat = ap_mat;
-    this.contacto = contacto;
-    switch (rangoInt) {
-      case 1:
-        this.Rango[0]=true;
-        break;
-      case 2:
-        this.Rango[1]=true;
-        break;
-      case 3:
-        this.Rango[2]=true;
-        break;
-    }
-  }
-  
-  // Sobrecarga del contructor para la base de datos.
-  public Tatuador(int id, String nombre, String ap_pat, String ap_mat, String contacto, int rangoInt){
-    this.id = id;
-    this.nombre = nombre;
-    this.ap_pat = ap_pat;
-    this.ap_mat = ap_mat;
-    this.contacto = contacto;
-    switch (rangoInt) {
-      case 1:
-        this.Rango[0]=true;
-        break;
-      case 2:
-        this.Rango[1]=true;
-        break;
-      case 3:
-        this.Rango[2]=true;
-        break;
-    }
-  }
-  
-  public int getId(){
-        return id;
-  }
-  public String getNombre() {
-        return nombre;
-  }
-  public String getAp_pat() {
-      return ap_pat;
-  }
-  public String getAp_mat() {
-      return ap_mat;
-  }
-  public String getContacto() {
-      return contacto;
-  }
-    public int getRango() {
-      return rangoInt;
-  }
-    public void setNombre(String nombre) {
+    public Tatuador(String nombre, String ap_pat, String ap_mat, String contacto, int rangoInt){
       this.nombre = nombre;
-  }
-    public void setAp_pat(String ap_pat) {
       this.ap_pat = ap_pat;
-  }
-    public void setAp_mat(String ap_mat) {
       this.ap_mat = ap_mat;
-  }
-    public void setContacto(String contacto) {
       this.contacto = contacto;
-  }
+      switch (rangoInt) {
+        case 1:
+          this.Rango[0]=true;
+          break;
+        case 2:
+          this.Rango[1]=true;
+          break;
+        case 3:
+          this.Rango[2]=true;
+          break;
+      }
+    }
+
+    // Sobrecarga del contructor para la base de datos.
+    public Tatuador(int id, String nombre, String ap_pat, String ap_mat, String contacto, int rangoInt){
+        this.id = id;
+        this.nombre = nombre;
+        this.ap_pat = ap_pat;
+        this.ap_mat = ap_mat;
+        this.contacto = contacto;
+        switch (rangoInt) {
+          case 1:
+            this.Rango[0]=true;
+            break;
+          case 2:
+            this.Rango[1]=true;
+            break;
+          case 3:
+            this.Rango[2]=true;
+            break;
+        }
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getAp_pat() {
+        return ap_pat;
+    }
+    
+    public String getAp_mat() {
+        return ap_mat;
+    }
+    
+    public String getContacto() {
+        return contacto;
+    }
+    
+    public int getRango() {
+        return rangoInt;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setAp_pat(String ap_pat) {
+        this.ap_pat = ap_pat;
+    }
+    
+    public void setAp_mat(String ap_mat) {
+        this.ap_mat = ap_mat;
+    }
+    
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    
     public void setRango(int rango) {
-      this.rangoInt = rango;
-  }
+        this.rangoInt = rango;
+    }
 }
