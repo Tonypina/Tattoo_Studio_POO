4b51840d7f4c31cd1bd4cd301ebfbbd4e44f0ca0
Pedro2606 <54335044+Pedro2606@users.noreply.github.com>
2021-01-26 16:38:46 -0600

    Update Socio.java

--- src/objects/Socio.java
+++ src/objects/Socio.java
@@ -5,19 +5,26 @@ public class Socio{
     private String nombre;
     private String contacto;
     private boolean rango;
+    private double total;
 
-    public Socio(String nombre, String contacto, boolean rango){
+    public Socio(String nombre, String contacto, boolean rango, double total){
         this.nombre = nombre;
         this.contacto = contacto;
         this.rango = rango;
+	this.total=total;
     }
 
     // Sobrecarga de constructor para base de datos
-    public Socio(int id, String nombre, String contacto, boolean rango){
+    public Socio(int id, String nombre, String contacto, boolean rango, double total){
         this.id = id;
         this.nombre = nombre;
         this.contacto = contacto;
         this.rango = rango;
+	this.total=total;
+    }
+	
+    public double getTotal(){
+	    return total;
     }
 
     public int getId(){
@@ -28,6 +35,9 @@ public class Socio{
         return nombre;
     }
 	
+    public void setTotal(double total){
+	    this.total=total;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
