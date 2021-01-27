f9831fdda8616dd3b77ddce244c51cc6c33bbc94
Eder-Berno <61462407+Eder-Berno@users.noreply.github.com>
2021-01-26 18:09:18 -0600

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,61 +1,69 @@
 package controller;
+import Model.*;
+import view.*;
 import objects.*;
 
-class Tatuador{
-  public boolean[] Rango = [Principal, Aprendiz, Secundario];
-  public String Nombre;
-  Tatuador(String[] Nombre, int Rango){
-    this.Nombre = Nombre
-    switch (Rango) {
+class Controladora{
+  private double comiclip = 0.0417; //Debe ser versátil. Preguntar si el IVA está incluído en los costos, que pedo con el IVA
+
+  public void ProcesoTicket(Ticket t){ //Ticket.costo debe ser la suma del monto correspondiente a la suma del costo del tatuaje y de la mercancía
+    double monto;
+    double comision;
+    double proveedor;
+    if (t.clip) {
+      t.costo -= t.costo*comiclip;
+    }
+    if (t.merch){
+      for(int count = 0; count < t.productos.size(); count++){
+//Necesitamos un método getProveedores que regrese un ArrayList de proveedores
+        if (t.productos[count].proveedor.equals("BLK")){ //Comentar en reunión
+          t.costo -= t.productos[count].precioPro;  //Reducir Stock
+          monto += t.productos[count].precioPro*(1-margen);
+          proveedorBLK += t.productos[count].precioPro*margen; //Es neceseria una clase proveedor que contenga un String name, double total, double margen.
+          t.productos[count].cantidadPro -= 1;
+        }
+      }
+    }
+    switch(t.Tatuador.rangoInt){
       case 1:
-        this.Principal=1;
+        comision = 0.6;
+        break;
       case 2:
-        this.Secundario=1;
-      case 3:
-        this.Aprendiz=1;
+        comision = 0.5;
+        break;
+      case 3:         //Comentar con Vista
+        if(t.visita){ //Solo el tatuador secundario tiene la opción de cobrar más por llevar clientes.
+          comision = 0.6;
+        }else{
+          comision = 0.4;
+        }
+        break;
     }
-    public int getPortencaje(){
+    monto += t.costo - t.costo*comision;
+    Model.aumentarGanancia(monto);
+    t.tatuador.total += comision*t.costo;
+    Model.modificarTatuador(t.Tatuador);
+    Model.aumentarPagoProveedores(proveedorBLK); //Comentar con Modelo. Se puede pasar proveedor y monto
+  }
 
+  public void procesoGanancias(){
+    private double pago;
+    private double total = Model.obtenerGanancias();
+    private ArrayList<Socio> socios = Model.getSocios();
+    for(int counter = 0; counter < socios.size(); counter++){
+      if(socios[counter].rango){
+        comision = 0.35; //¿qué pasaría si un socio se añade y se va?
+      }else{
+        comision = 0.15;
+      }
+      pago += total*comision;
+      total -= pago;
+      socios[counter].total += pago;
+      Model.modificarSocio(socios[counter]);
+      Model.aumentarGanancias(-pago);
     }
+    Model.aumentarReinversion(total);
   }
-}
-
-class Socio{
-  public boolean Rango; //0 para minoritario, 1 para mayoritario
-}
-class Gestor extends Controladora{
-    
-}
-
-class Controladora{ //Todo lo público es realizable por el gestor. Los métodos privados son ezclusivos del dueño
-  ArrayList<Tatuador> Tatuadores = new ArrayList<Tatuador>(); //
-  private void RegistroSocio(){
-
-  }
-  private void RegistroTatuador(String[] Nombre, int Rango){
-    Tatuador temp = new Tatuador(Nombre, Rango);
-    Tatuadores.add(temp);
-  }
-  private void EliminarSocio(){
-
-  }
-  private void EliminarTatuador(Tatuador funado){
-
-  }
-  private MostrarReporte(){ //Solo le pasamos la información a la vista
-
-  }
-  public MostrarCitas(){ //Solo le pasamos la información a la vista
-
-  }
-  public void Ingresar(){ //Solo le pasamos la información a la vista?
-
-  }
-  public Tatuador[] ListaTatuadores(){
-
-  }
-  public void ProcesoTicket(Ticket){
-    if (Tatuador.equals(Jose)) {
-
-    }
-}
+/*Para hacer el programa dinámico, debemos pedir el porcentaje de cada socio.
+También necesitamos el margen de utilidad de cada proveedor si es que se desea
+registrar más de 2.
