27ca2b45216be6bd4a6c41967be93ac65a11bbde
Pedro2606 <54335044+Pedro2606@users.noreply.github.com>
2021-01-26 18:26:00 -0600

    Add files via upload

---
+++ src/objects/Proveedor.java
@@ -0,0 +1,57 @@
+package objects;
+public class Proveedor{
+	private int id;
+    	private String nombre;
+    	private String contacto;
+    	private double margen;
+    	private double total;
+
+	public Proveedor(String nombre, String contacto, double margen, double total){
+		this.nombre = nombre;
+       		this.contacto = contacto;
+        	this.margen = margen;
+		this.total=total;
+	}
+	//Sobrecarga de constructor
+	public Proveedor(int id, String nombre, String contacto, double margen, double total){
+		this.id =id;
+		this.nombre = nombre;
+       		this.contacto = contacto;
+        	this.margen = margen;
+		this.total=total;
+	}
+	public double getTotal(){
+		return total;
+    	}
+
+    	public int getId(){
+        	return id;
+    	}
+	
+    	public String getNombre() {
+        	return nombre;
+    	}
+	
+    	public void setTotal(double total){
+	    	this.total=total;
+    	}
+    	public void setNombre(String nombre) {
+        	this.nombre = nombre;
+    	}
+	
+    	public String getContacto() {
+    		return contacto;
+    	}
+	
+    	public void setContacto(String contacto) {
+    		this.contacto = contacto;
+    	}
+	
+    	public double getMargen() {
+    		return margen;
+    	}
+	
+    	public void setMargen(double margen) {
+    		this.margen = margen;
+    	}
+}
