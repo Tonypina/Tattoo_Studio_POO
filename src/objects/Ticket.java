fd27651c0d23ee866342d09502c35e2ec786545c
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-02-09 14:58:11 -0500

    Add files via upload

--- src/objects/Ticket.java
+++ src/objects/Ticket.java
@@ -13,28 +13,43 @@ public class Ticket {
     private int id;
     private boolean clip;
     private boolean prod;
+    private double subtotalTatuaje;
+    private double subtotalPerforacion;
     private double total;
+    private int dia;
+    private int mes;
+    private int anio;
     private Tatuador tatuador;
+    private Proveedor perforador;
     private boolean visita;
     private ArrayList<Producto> productos;
-    
 
-    public Ticket(int id, boolean clip, boolean prod,  double total, Tatuador tatuador, boolean visita, ArrayList<Producto> productos) {
+    // Sobrecarga de constructor para base de datos
+
+    public Ticket(int id, boolean clip, boolean prod, double subtotalTatuaje, double subtotalPerforacion, double total, int dia, int mes, int anio, Tatuador tatuador, Proveedor perforador, boolean visita, ArrayList<Producto> productos) {
         this.id = id;
         this.clip = clip;
         this.prod = prod;
+        this.subtotalTatuaje = subtotalTatuaje;
+        this.subtotalPerforacion = subtotalPerforacion;
         this.total = total;
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
         this.tatuador = tatuador;
+        this.perforador = perforador;
         this.visita = visita;
         this.productos = productos;
     }
 
-    // Sobrecarga de constructor para la Base de Datos.
-    public Ticket(boolean clip, boolean prod,  double total, Tatuador tatuador, boolean visita, ArrayList<Producto> productos) {
+    public Ticket(boolean clip, boolean prod, double subtotalTatuaje, double subtotalPerforacion, double total, Tatuador tatuador, Proveedor perforador, boolean visita, ArrayList<Producto> productos) {
         this.clip = clip;
         this.prod = prod;
+        this.subtotalTatuaje = subtotalTatuaje;
+        this.subtotalPerforacion = subtotalPerforacion;
         this.total = total;
         this.tatuador = tatuador;
+        this.perforador = perforador;
         this.visita = visita;
         this.productos = productos;
     }
@@ -45,6 +60,34 @@ public class Ticket {
     
     public void setVisita(boolean visita){
         this.visita = visita;
+    }
+
+    public void setSubtotalTatuaje(double subtotalTatuaje) {
+        this.subtotalTatuaje = subtotalTatuaje;
+    }
+
+    public void setSubtotalPerforacion(double subtotalPerforacion) {
+        this.subtotalPerforacion = subtotalPerforacion;
+    }
+
+    public double getSubtotalTatuaje() {
+        return subtotalTatuaje;
+    }
+
+    public double getSubtotalPerforacion() {
+        return subtotalPerforacion;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
     }
     
     
@@ -70,6 +113,10 @@ public class Ticket {
     public Tatuador getTatuador() {
         return tatuador;
     }
+    
+    public Proveedor getPerforador(){
+        return perforador;
+    }
 
     public ArrayList<Producto> getProductos() {
         return productos;
@@ -94,6 +141,10 @@ public class Ticket {
 
     public void setTatuador(Tatuador tatuador) {
         this.tatuador = tatuador;
+    }
+    
+    public void setPerforador(Proveedor perforador){
+        this.perforador = perforador;
     }
 
     public void setProductos(ArrayList<Producto> productos) {
