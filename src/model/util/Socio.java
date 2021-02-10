d83a25877fa3f1ea72e17274af81af057672ec37
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2021-02-09 20:58:00 -0500

    Add files via upload

---
+++ src/model/util/Socio.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+/**
+ *
+ * @author Airam
+ */
+public class Socio {
+    private int id;
+    private String nombre;
+    private String contacto;
+    private boolean rango;
+    private double total;
+
+    public Socio(String nombre, String contacto, boolean rango, double total){
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.rango = rango;
+	this.total=total;
+    }
+
+    // Sobrecarga de constructor para base de datos
+    public Socio(int id, String nombre, String contacto, boolean rango, double total){
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.rango = rango;
+	this.total=total;
+    }
+	
+    public double getTotal(){
+	    return total;
+    }
+
+    public int getId(){
+        return id;
+    }
+	
+    public String getNombre() {
+        return nombre;
+    }
+	
+    public void setTotal(double total){
+	    this.total=total;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+	
+    public String getContacto() {
+    	return contacto;
+    }
+	
+    public void setContacto(String contacto) {
+    	this.contacto = contacto;
+    }
+	
+    public boolean isRango() {
+    	return rango;
+    }
+	
+    public void setRango(boolean rango) {
+    	this.rango = rango;
+    }
+}
