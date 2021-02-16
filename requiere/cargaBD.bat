3bc66128878a42aa7ad980733bcc99633215570a
Jorge Hernandez <kokherlo@hotmail.com>
2021-02-15 18:43:05 -0600

    Add files via upload

---
+++ requiere/cargaBD.bat
@@ -0,0 +1,7 @@
+@echo
+
+echo Creando base de datos y cargando datos...
+
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u babayaga -h localhost < .\comp\babayaga_db.sql
+
+echo BD cargada exitosament...
